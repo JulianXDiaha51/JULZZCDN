6e888c3a275f0d13e0dab59ab3a7bea2e88db7c0
JulzPiw <126005230+JulianXDiaha51@users.noreply.github.com>
2025-12-04 16:41:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteName: "JulzzCDN",
+  authorName: "JulianDev",
+  profilePicture: "https://c.termai.cc/i31/L7R.jpg",
+  profileBanner: "https://cihuy.biz.id/file/aOvxOyC-.gif",
+  aboutText: "Proyek pribadi yang menghadirkan layanan file hosting dan pemendek URL dengan desain yang sederhana, cepat, dan efisien.",
+  backgroundImage: "https://cihuy.biz.id/file/7W7qtMDY.jpg"
+};
